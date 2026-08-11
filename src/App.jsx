a52a79c9d8dcf1921a04ef80073d7188086c49be
fajarworks <muhammad.fajarr362@gmail.com>
2026-08-12 01:36:33 +0700

    feat: wrap routes in MainLayout and add Home route

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,8 @@ import FetchPokemon from './pages/FetchPokemon'
 import Counter from './pages/Counter'
 import { Route, Routes } from 'react-router'
 import Product from './pages/Product'
+import Home from './pages/Home'
+import MainLayout from './assets/layout/MainLayout'
 
 
 
@@ -9,9 +11,12 @@ import Product from './pages/Product'
 function App() {
   return (
     <Routes>
+        <Route element={<MainLayout/>} >
+        <Route path='/' element= {<Home/>}/>
         <Route path='/counter' element={<Counter/>}/>
         <Route path='/fetch-pokemon' element={<FetchPokemon/>}/>
         <Route path='/product' element={<Product/>} />
+        </Route>
     </Routes>
   )
 }
