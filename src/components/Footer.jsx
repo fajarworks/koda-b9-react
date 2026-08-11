96b05d647642067c7204037a739e65bec5d4fc8b
fajarworks <muhammad.fajarr362@gmail.com>
2026-08-12 01:36:56 +0700

    feat: add Footer component with basic structure

---
+++ src/components/Footer.jsx
@@ -0,0 +1,11 @@
+    // import React from 'react'
+
+    const Footer = () => {
+    return (
+        <footer className="p-3 bg-orange-700">
+            ini footer
+        </footer>
+    )
+    }
+
+    export default Footer
