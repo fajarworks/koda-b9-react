69302d19611c4d5ba653f26a3bd1c1b8cff65eae
fajarworks <muhammad.fajarr362@gmail.com>
2026-08-12 13:16:49 +0700

    feat: fix import file header and footer

--- src/layout/MainLayout.jsx
+++ src/layout/MainLayout.jsx
@@ -1,7 +1,6 @@
-// import React from 'react'
 import { Outlet } from 'react-router'
-import Header from '../../components/Header'
-import Footer from '../../components/Footer'
+import Header from '../components/Header.jsx'
+import Footer from '../components/Footer.jsx'
 
 const MainLayout = () => {
   return (
