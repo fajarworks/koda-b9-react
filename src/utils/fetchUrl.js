289f0aa35cb6922d9c387ebe541cfc5d7decceb0
fajarworks <muhammad.fajarr362@gmail.com>
2026-08-12 13:14:49 +0700

    docs: add js docs for documentation code

--- src/utils/fetchUrl.js
+++ src/utils/fetchUrl.js
@@ -1,8 +1,16 @@
-const  fetchData = async(url, options={}) =>{
+/**
+ *  Wrapper for Fetchiing URL
+ * @param {string} url
+ * @param {RequestInit} options
+ * @returns {Promise<*>}
+ */
+
+
+const fetchData = async (url, options = {}) => {
     const res = await fetch(url, options)
     if (!res.ok) throw new Error(res.status)
     const data = res.json()
-    
+
     return data
 
 }
