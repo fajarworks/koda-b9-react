dee260ca7f796b1a94f4ddf27f58d5877f5e0337
fajarworks <muhammad.fajarr362@gmail.com>
2026-08-14 22:17:04 +0700

    feat: add routing for detail page pokemon

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,29 @@
-import FetchPokemon from './pages/FetchPokemon'
-import Counter from './pages/Counter'
-import { Route, Routes } from 'react-router'
-import Product from './pages/Product'
-import Home from './pages/Home'
-import MainLayout from './assets/layout/MainLayout'
-
-
-
+import FetchPokemon from "./pages/FetchPokemon";
+import Counter from "./pages/Counter";
+import { Route, Routes } from "react-router";
+import Product from "./pages/Product";
+import Home from "./pages/Home";
+import MainLayout from "./layout/MainLayout";
+import DetailPage from "./pages/DetailPage";
+// import Profile from './pages/ProfileForm.jsx'
 
 function App() {
   return (
     <Routes>
-        <Route element={<MainLayout/>} >
-        <Route path='/' element= {<Home/>}/>
-        <Route path='/counter' element={<Counter/>}/>
-        <Route path='/fetch-pokemon' element={<FetchPokemon/>}/>
-        <Route path='/product' element={<Product/>} />
+      <Route path="/" element={<MainLayout />}>
+        <Route index element={<Home />} />
+        <Route path="counter" element={<Counter />} />
+        {/* <Route path="pokemon" element={<FetchPokemon />} />*/}
+
+        <Route path="pokemon">
+          <Route index element={<FetchPokemon />} />
+          <Route path=":id" element={<DetailPage />} />
         </Route>
+
+        <Route path="product" element={<Product />} />
+      </Route>
     </Routes>
-  )
+  );
 }
 
-export default App
+export default App;
