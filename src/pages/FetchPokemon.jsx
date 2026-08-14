e8be0e8980dea4d0ca0c968f153fb54bce432ef3
fajarworks <muhammad.fajarr362@gmail.com>
2026-08-14 23:47:25 +0700

    feat:add filter based on types and name

--- src/pages/FetchPokemon.jsx
+++ src/pages/FetchPokemon.jsx
@@ -1,77 +1,170 @@
-import React from 'react'
-import fetchData from '../utils/fetchUrl'
+import React from "react";
+import fetchData from "../utils/fetchUrl";
+import { Link, useSearchParams } from "react-router";
 
 function FetchPokemon() {
-    const [data, setData] = React.useState([])
-    const [search, setSearch] = React.useState("")
-    // console.log(data)
-    React.useEffect(()=>{
-        (async function(){
-            try{
-                const data = await fetchData("https://pokeapi.co/api/v2/pokemon?limit=40&offset=0")
-                const {results} = data
-                const detailPokemon = results.map(async (a)=>{
-                    const detail = await fetchData(a.url)
-                    // console.log(detail)
-                    const {name, types, sprites} = detail
-                    const type = types.map((t)=> t.type.name)
-                    const sprite = sprites.front_default
-                    return {
-                        name,
-                        type,
-                        sprite
-                    }
-                })
-                const pokemons = await Promise.all(detailPokemon)
-                setData(pokemons)
-            }catch(err){
-                console.error(err.message)
-            }
-        })()
-    },[])
+  const [data, setData] = React.useState([]);
+  const [search, setSearch] = React.useState("");
+  const [types, setTypes] = React.useState([]);
+  const [searchParams, setSearchParams] = useSearchParams();
 
-    function handleInput(e) {
-        e.preventDefault()
-        const data = new FormData(e.target)
-        const value = data.get("name")
-        console.log(value)     
-        setSearch(value.toLocaleLowerCase())
-        e.target.reset()
-    } 
+  const q = searchParams.get("name") || ""
+  const t = searchParams.get("type") || ""
 
-    const filterChar = data.filter((c)=> c.name.toLowerCase().includes(search))
-    console.log(filterChar)
+  const handleSearch = (e) => {
+    const value = e.target.value
+    if (value) {
+      searchParams.set("name", value)
+    } else {
+      searchParams.delete("name")
+    }
+    setSearchParams(searchParams)
+  }
+
+  const handleType = (type) => {
+    if (t === type) {
+      searchParams.delete("type")
+    } else {
+      searchParams.set("type", type)
+    }
+    setSearchParams(searchParams)
+  }
+
+  React.useEffect(() => {
+    (async function () {
+      try {
+        const data = await fetchData(
+          "https://pokeapi.co/api/v2/pokemon?limit=100&offset=0",
+        );
+        const { results } = data;
+        const detailPokemon = results.map(async (a) => {
+          const detail = await fetchData(a.url);
+          const { name, types, sprites, id } = detail;
+          const type = types.map((t) => t.type.name);
+          const sprite = sprites.front_default;
+          return {
+            name,
+            type,
+            sprite,
+            id,
+          };
+        });
+        const pokemons = await Promise.all(detailPokemon);
+        setData(pokemons);
+      } catch (err) {
+        console.error(err.message);
+      }
+    })();
+
+    (async function () {
+      try {
+        const data = await fetchData("https://pokeapi.co/api/v2/type?limit=20");
+        setTypes(data);
+      } catch (err) {
+        console.error(err.message);
+      }
+    })();
+  }, []);
+
+
+
+  const filterChar = data.filter((pokemon) => {
+    const matchName = pokemon.name.includes(q);
+
+    const matchType = t
+      ? pokemon.type.includes(t)
+      : true;
+
+
+    console.log(matchName, matchType);
+
+    return matchName && matchType;
+  });
 
   return (
     <>
-    <section className='max-w-7xl mx-auto min-h-screen p-8'>
-        <h1 className='text-center my-2 font-bold text-2xl'>POKEMON CHARACTERS</h1>
-    <form onSubmit={handleInput} className='justify-center mb-5 w-full'>
-        <div className='flex'>
-        {/* <label htmlFor="name" className='text-center'>POKEMON CHARACTERS</label> */}
-        <input className=' w-full border rounded-md px-2 py-1' type="text" name="name" id="name" />
-        <button className='min-w-20 cursor-pointer bg-green-500 rounded-lg'>Search</button>
+      <section className="max-w-7xl mx-auto min-h-screen p-8">
+        <h1 className="text-center my-2 font-bold text-2xl">
+          POKEMON CHARACTERS
+        </h1>
+        <form  className="justify-center mb-5 w-full">
+          <div className="flex">
+            <input
+            onChange={handleSearch}
+              className=" w-full border rounded-md px-2 py-1"
+              type="text"
+              name="name"
+              id="name"
+            />
+            <button className="min-w-20 cursor-pointer bg-green-500 rounded-lg">
+              Search
+            </button>
+          </div>
+        </form>
+        <div className="flex items-center justify-center">
+          {types.results?.map((type) => {
+                   const isActive = t === type.name;
+
+                   return (
+                     <button
+                       type="button"
+                       key={type.name}
+                       onClick={() => handleType(type.name)}
+                       className={`px-2 py-0.5 rounded-full cursor-pointer capitalize ${
+                         isActive
+                           ? "bg-orange-500 text-white"
+                           : "bg-orange-200 text-orange-500"
+                       }`}
+                     >
+                       {type.name}
+                     </button>
+                   );
+                 })}
         </div>
-    </form>
-    <div className='grid grid-cols-4 gap-3'>
-    {filterChar.length <= 0 ? (<p>{search} tidak ditemukan</p>) : 
-            filterChar.map((item, idx)=>{
-        return(            
-            <article className='border rounded-lg flex justify-between  items-center gap-2' key={idx}>
-                <div className=''>
-                <img className='w-50' src={item.sprite} alt={item.name} />       
-                </div>
-                <div className='flex flex-col w-full gap-2 text-center'>
-                <p className=''>{item.name}</p>
-                <p>{item.type}</p>
-                </div>
-            </article>
-        )
-    })}
-    </div>
-    </section>
+        <div className="grid grid-cols-4 gap-3">
+          {filterChar.length <= 0 ? (
+            <p>{search} tidak ditemukan</p>
+          ) : (
+            filterChar.map((item) => {
+              return (
+                <article
+                  className="border border-orange-50 bg-orange-100 rounded-lg flex justify-between items-center gap-2"
+                  key={item.id}
+                >
+                  <Link
+                    to={`/pokemon/${item.id}`}
+                    className="flex items-center justify-center w-full"
+                  >
+                    <div>
+                      <img className="w-50" src={item.sprite} alt={item.name} />
+                    </div>
+
+                    <div className="flex flex-col w-full gap-2 text-center ">
+                      <p className="font-semibold capitalize">{item.name}</p>
+
+                      <div className="flex items-center justify-center gap-2">
+                        {item.type.map((t) => {
+                          return (
+                            <span
+                              className="bg-orange-500 text-white px-3 py-1 rounded-lg text-sm capitalize"
+                              key={t}
+                              onClick={() => handleType(t.name)}
+                            >
+                              {t}
+                            </span>
+                          );
+                        })}
+                      </div>
+                    </div>
+                  </Link>
+                </article>
+              );
+            })
+          )}
+        </div>
+      </section>
     </>
-  )
+  );
 }
 
-export default FetchPokemon
+export default FetchPokemon;
