e5bc7ae53caddf8e6ff480f8da2e04309d330bd3
fajarworks <muhammad.fajarr362@gmail.com>
2026-08-19 23:52:31 +0700

    feat: create sign in button and display name user login

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,21 +1,41 @@
 // import React from 'react'
-import { Link } from 'react-router'
+import { useContext } from "react";
+import { Link } from "react-router";
+import userContext from "../contexts/userContext.js";
 const Header = () => {
-  return (
-    <header className='bg-amber-700 '>
-        <nav className='flex justify-between p-2 text-white font-medium'>
-            <div>
-                LOGO
-            </div>
-            <ul className='flex gap-2'>
-                <li> <Link to={"/"}>Home</Link></li>
-                <li> <Link to={"/counter"}>Counter Button</Link></li>
-                <li> <Link to={"/product"}>Input Product</Link></li>
-                <li> <Link to={"/fetch-pokemon"}>Fetch Pokemon</Link></li>
-            </ul>
-        </nav>
-    </header>
-  )
-}
+  const { state, dispatch } = useContext(userContext);
 
-export default Header
+  return (
+    <header className="bg-amber-700 ">
+      <nav className="flex justify-between p-2 text-white font-medium">
+        <div>{state.login?.username}</div>
+        <ul className="flex gap-2">
+          <li>
+            {" "}
+            <Link to={"/"}>Home</Link>
+          </li>
+          <li>
+            {" "}
+            <Link to={"/counter"}>Counter Button</Link>
+          </li>
+          <li>
+            {" "}
+            <Link to={"/product"}>Input Product</Link>
+          </li>
+          <li>
+            {" "}
+            <Link to={"/pokemon"}>Fetch Pokemon</Link>
+          </li>
+          <li>
+            {" "}
+            <Link to={"/profile"}>Profile</Link>
+          </li>
+
+          {state.login?.username ? <span className="cursor-pointer bg-white text-orange-500" onClick={()=>dispatch({type:"LOGOUT"})}>Sign Out</span> : <Link className=" bg-white text-orange-500 rounded-full py-0.5 px-1" to="/login">Sign In</Link>}
+        </ul>
+      </nav>
+    </header>
+  );
+};
+
+export default Header;
